878bbf1554ed3b868c79cda15f38a1ac3f87ebcb
Jialai Li <oliver@Jialais-MacBook-Pro.local>
2020-05-14 17:39:54 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
+ //Replaces componentDidMount and componentWillUnmount
+ useEffect(() => {
+  var timerID = setInterval( () => tick(), 1000 );
+
+  return function cleanup() {
+      clearInterval(timerID);
+    };
+ });
+
+   function tick() {
+    setDate(new Date());
+   }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      我是李佳来， 今天是{date.toLocaleTimeString()}
     </div>
   );
 }
