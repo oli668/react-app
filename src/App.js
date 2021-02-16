84bb6a026fa8485459db9c5371da771b44713192
oli668 <37321582+oli668@users.noreply.github.com>
2021-02-16 17:57:24 -0500

    add home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import logo from "./logo.svg";
 import "./App.css";
 import Button from "antd/es/button";
+import { HomePage } from "./pages/HomePage/home";
 
-function App() {
+const App = () => {
   const { login } = useSelector(state => state.login);
   const [date, setDate] = useState(new Date());
 
@@ -23,9 +23,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <HomePage />
       <Button type="primary">Button</Button>
       我是李佳来， 今天是{date.toLocaleTimeString()} {login}
     </div>
