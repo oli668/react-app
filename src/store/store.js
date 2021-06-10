c0f28be07b290d2fcad57aaefd425175ba2665eb
oli668 <37321582+oli668@users.noreply.github.com>
2021-06-10 18:29:00 +0800

    fixed refresh page bug

--- src/store/store.js
+++ src/store/store.js
@@ -10,7 +10,7 @@ const persistConfig = {
   // configuration object for redux-persist
   key: "root",
   storage, // define which storage to use,
-  whitelist: ["cocktailsDetails"],
+  whitelist: ["cocktailsDetails", "filterCocktails"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer); // create a persisted reducer
