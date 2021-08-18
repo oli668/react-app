8461286ff835432dd2b62be3cbccdf72ad77c29e
oli668 <37321582+oli668@users.noreply.github.com>
2021-08-18 21:24:22 +0800

    modify footer css

--- src/pages/IntroductionPage/IntroductionPage.jsx
+++ src/pages/IntroductionPage/IntroductionPage.jsx
@@ -82,12 +82,11 @@ export const IntroductionPage = () => {
               <li className="p-2 cursor-pointer hover:underline">
                 用户服务协议
               </li>
-              <li className="p-2 cursor-pointer hover:underline">广告投放</li>
               <li className="p-2 cursor-pointer hover:underline">隐私政策</li>
               <li className="p-2 cursor-pointer hover:underline">联系我们</li>
               <li className="p-2 cursor-pointer hover:underline">加入我们</li>
             </ul>
-            <div className="flex mx-auto text-white text-center">
+            <div className="flex mx-auto text-white text-center flex-col md:flex-row">
               <p>咕噜铁汁科技（杭州）有限公司 ©2021</p>
               <p>
                 <span className="px-4">浙ICP备2021023461 工信部ICP备案</span>
