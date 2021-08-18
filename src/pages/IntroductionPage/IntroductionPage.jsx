5b1111fc12ca9daeddc5d127c4dcd4368aa8359b
oli668 <37321582+oli668@users.noreply.github.com>
2021-08-18 21:08:51 +0800

    modify css

--- src/pages/IntroductionPage/IntroductionPage.jsx
+++ src/pages/IntroductionPage/IntroductionPage.jsx
@@ -75,17 +75,19 @@ export const IntroductionPage = () => {
       </div>
       <Carousel></Carousel>
       <PresentImages></PresentImages>
-      <div class="bg-blue-dark">
-        <footer class="flex flex-wrap items-center justify-between p-3 m-auto">
-          <div class="container mx-auto flex flex-col flex-wrap items-center justify-between">
-            <ul class="flex mx-auto text-white text-center">
-              <li class="p-2 cursor-pointer hover:underline">用户服务协议</li>
-              <li class="p-2 cursor-pointer hover:underline">广告投放</li>
-              <li class="p-2 cursor-pointer hover:underline">隐私政策</li>
-              <li class="p-2 cursor-pointer hover:underline">联系我们</li>
-              <li class="p-2 cursor-pointer hover:underline">加入我们</li>
+      <div className="bg-blue-dark">
+        <footer className="flex flex-wrap items-center justify-between p-3 m-auto">
+          <div className="container mx-auto flex flex-col flex-wrap items-center justify-between">
+            <ul className="flex mx-auto text-white text-center">
+              <li className="p-2 cursor-pointer hover:underline">
+                用户服务协议
+              </li>
+              <li className="p-2 cursor-pointer hover:underline">广告投放</li>
+              <li className="p-2 cursor-pointer hover:underline">隐私政策</li>
+              <li className="p-2 cursor-pointer hover:underline">联系我们</li>
+              <li className="p-2 cursor-pointer hover:underline">加入我们</li>
             </ul>
-            <div class="flex mx-auto text-white text-center">
+            <div className="flex mx-auto text-white text-center">
               <p>咕噜铁汁科技（杭州）有限公司 ©2021</p>
               <p>
                 <span className="px-4">浙ICP备2021023461 工信部ICP备案</span>
